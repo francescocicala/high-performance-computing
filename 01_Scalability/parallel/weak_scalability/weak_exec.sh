de649b41b485e92c1f106443b2b40372f53ad369
francescocicala <francesco.cicala00@gmail.com>
2019-01-07 10:26:06 +0100

    modified weak_exec.sh

--- 01_Scalability/parallel/weak_scalability/weak_exec.sh
+++ 01_Scalability/parallel/weak_scalability/weak_exec.sh
@@ -9,10 +9,24 @@ echo
 echo "WEAK SCALABILITY SCRIPT"
 echo
 
+FOLDER="/home/fcicala/high-performance-computing/01_Scalability/parallel/weak_scalability/"
+cd "$FOLDER"
+
 for procs in {1..20}
 do
-  mpirun -np $procs /home/fcicala/high-performance-computing/01_Scalability/parallel/weak_scalability/mpi_weak.x 10000000
+  mpirun -np $procs mpi_weak.x 100000
 done 
+mv weak_scal.dat weak_scal5.dat
 
-mv weak_scal.dat /home/fcicala/high-performance-computing/01_Scalability/parallel/weak_scalability/weak_scal7.dat
+for procs in {1..20}
+do
+  mpirun -np $procs mpi_weak.x 1000000
+done 
+mv weak_scal.dat weak_scal6.dat
 
+
+for procs in {1..20}
+do
+  mpirun -np $procs mpi_weak.x 10000000
+done 
+mv weak_scal.dat weak_scal7.dat
