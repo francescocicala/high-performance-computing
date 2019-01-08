c68bbc37e89f1da6a179feca294c01ac807567bf
francescocicala <francesco.cicala00@gmail.com>
2019-01-08 08:30:12 +0100

    strong_exec.sh modified

--- 01_Scalability/parallel/strong_scalability/strong_exec.sh
+++ 01_Scalability/parallel/strong_scalability/strong_exec.sh
@@ -11,7 +11,6 @@ FOLDER="/home/fcicala/high-performance-computing/01_Scalability/parallel/strong_
 cd "$FOLDER"
 
 N=100000
-
 for procs in {1..20}
 do
   iter_per_proc=$(bc <<< "scale = 4; $N/$procs")
@@ -25,4 +24,38 @@ do
   echo
   mpirun -np $procs mpi_strong.x $iter_per_proc
 done 
+
+
+N=1000000
+for procs in {1..20}
+do
+  iter_per_proc=$(bc <<< "scale = 4; $N/$procs")
+  iter_per_proc=${iter_per_proc%.*} 
+  echo
+  echo "/////////////////////////////////////////"
+  echo "RUN $procs:"
+  echo "Total Iterations: $N"
+  echo "Iterations per processor: $iter_per_proc"
+  echo "/////////////////////////////////////////"
+  echo
+  mpirun -np $procs mpi_strong.x $iter_per_proc
+done 
+
+
+N=10000000
+for procs in {1..20}
+do
+  iter_per_proc=$(bc <<< "scale = 4; $N/$procs")
+  iter_per_proc=${iter_per_proc%.*} 
+  echo
+  echo "/////////////////////////////////////////"
+  echo "RUN $procs:"
+  echo "Total Iterations: $N"
+  echo "Iterations per processor: $iter_per_proc"
+  echo "/////////////////////////////////////////"
+  echo
+  mpirun -np $procs mpi_strong.x $iter_per_proc
+done 
+
+echo "COMPLETED"
 
