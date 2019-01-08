a605711428d51eb8de0dde36709cd40f6f287147
Francesco Cicala <francesco.cicala00@gmail.com>
2019-01-08 08:52:03 +0100

    fix conflict

--- 01_Scalability/parallel/strong_scalability/strong_exec.sh
+++ 01_Scalability/parallel/strong_scalability/strong_exec.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 
 module load openmpi
-<<<<<<< .merge_file_ScPTxA
-
-=======
 find . -type f -name strong_exec.sh.\* -exec rm {} \;
->>>>>>> .merge_file_tnRLPx
+
 
 echo
 echo "STRONG SCALABILITY SCRIPT"
@@ -27,12 +24,8 @@ do
   echo "/////////////////////////////////////////"
   echo
   mpirun -np $procs mpi_strong.x $iter_per_proc
-done 
-<<<<<<< .merge_file_ScPTxA
-<<<<<<< .merge_file_G9Wkkt
-=======
+done
 mv strong_scal.dat strong_scal5.dat
->>>>>>> .merge_file_tnRLPx
 
 
 N=1000000
@@ -68,50 +61,5 @@ do
 done 
 mv strong_scal.dat strong_scal7.dat
 
-
-echo "COMPLETED"
-
-
-N=10000000
-=======
-mv strong_scal.dat strong_scal5.dat
-
-
-N=1000000
->>>>>>> .merge_file_f71Esq
-for procs in {1..20}
-do
-  iter_per_proc=$(bc <<< "scale = 4; $N/$procs")
-  iter_per_proc=${iter_per_proc%.*} 
-  echo
-  echo "/////////////////////////////////////////"
-  echo "RUN $procs:"
-  echo "Total Iterations: $N"
-  echo "Iterations per processor: $iter_per_proc"
-  echo "/////////////////////////////////////////"
-  echo
-  mpirun -np $procs mpi_strong.x $iter_per_proc
-done 
-mv strong_scal.dat strong_scal6.dat
-
-
-N=10000000
-for procs in {1..20}
-do
-  iter_per_proc=$(bc <<< "scale = 4; $N/$procs")
-  iter_per_proc=${iter_per_proc%.*} 
-  echo
-  echo "/////////////////////////////////////////"
-  echo "RUN $procs:"
-  echo "Total Iterations: $N"
-  echo "Iterations per processor: $iter_per_proc"
-  echo "/////////////////////////////////////////"
-  echo
-  mpirun -np $procs mpi_strong.x $iter_per_proc
-done 
-mv strong_scal.dat strong_scal7.dat
-
-
-echo "COMPLETED"
 
 echo "COMPLETED"
