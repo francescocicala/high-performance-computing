3b90fafd29c1c2d9fefe00a4b8c3387510550e0b
francescocicala <francesco.cicala00@gmail.com>
2019-01-09 11:54:33 +0100

    run

---
+++ Assignments/02_Profiling/clean.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+rm *.o
+rm persist.txt
+mv example1 example1.x
+mv example2 example2.x
+mv example3 example3.x
+mv example4 example4.x
+mv test test.x
--- Assignments/02_Profiling/readme.md
+++
@@ -1,3 +0,0 @@
-# Assignment 2 - Profiling
-
-Profiling a simple artificial neural network implementation in ANSI C by [GenANN](https://github.com/codeplea/genann).
